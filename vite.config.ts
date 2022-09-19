eff53e5524b93ec57303661dbe2473d8e8223548
Wlad Paiva <desenvolvedormg@gmail.com>
2022-09-19 15:17:31 -0300

    fix: build script

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,14 @@ import react from "@vitejs/plugin-react";
 // `@swan-bitcoin/xpub-lib` package needs those polyfills to work on the browser
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+import rollupNodePolyFill from "rollup-plugin-node-polyfills";
+import inject from "@rollup/plugin-inject";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
+    include: ["@swan-bitcoin/xpub-lib"],
     esbuildOptions: {
       plugins: [
         NodeGlobalsPolyfillPlugin({
@@ -16,6 +19,20 @@ export default defineConfig({
         }),
         NodeModulesPolyfillPlugin(),
       ],
+    },
+  },
+  resolve: {
+    alias: {
+      stream: "rollup-plugin-node-polyfills/polyfills/stream",
+      events: "rollup-plugin-node-polyfills/polyfills/events",
+      crypto: "crypto-browserify",
+      util: "rollup-plugin-node-polyfills/polyfills/util",
+      process: "rollup-plugin-node-polyfills/polyfills/process-es6",
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [inject({ Buffer: ["Buffer", "Buffer"] }), rollupNodePolyFill()],
     },
   },
 });
